a2b04d5d69f2d59cf86c17fcce414253e42b359a
Brennon Schow <brennonschow@Brennons-MacBook-Pro.local>
2020-03-29 21:32:24 -0600

    clean up js

--- scripts.js
+++ scripts.js
@@ -1,50 +1,38 @@
-const modals = document.getElementsByClassName('modal')
-const backgrounds = document.getElementsByClassName('modal-background')
-const closeButtons = document.getElementsByClassName('modal-close')
-const resources = document.getElementById('resources')
-const contact = document.getElementById('contact')
-const rModal = document.getElementById('resources-modal')
-const cModal = document.getElementById('contact-modal')
+const modals = document.querySelectorAll('.modal');
+const backgrounds = document.querySelectorAll('.modal-background');
+const closeButtons = document.querySelectorAll('.modal-close');
+const resources = document.querySelector('#resources');
+const contact = document.querySelector('#contact');
+const rModal = document.querySelector('#resources-modal');
+const cModal = document.querySelector('#contact-modal');
 
 function closeModals(){
-  Array.from(modals).map(modal => {
-    if (modal.classList.contains('is-active')){
-      modal.classList.remove('is-active')
-      return
-    }
-  })
+  modals.forEach(({ classList }) => {
+    classList.remove('is-active');
+  });
 }
 
-if (closeButtons) {
-  Array.from(closeButtons).map(button => {
-    button.addEventListener('click', closeModals)
-  })
-
-  Array.from(backgrounds).map(bg => {
-    bg.addEventListener('click', closeModals)
-  })
+if (closeButtons.length) {
+  [...closeButtons, ...backgrounds].forEach(el => {
+    el.addEventListener('click', closeModals);
+  });
 }
 
 resources.addEventListener('click', () => {
-  rModal.classList.add('is-active')
-})
+  rModal.classList.add('is-active');
+});
 
 contact.addEventListener('click', () => {
-  cModal.classList.add('is-active')
-})
+  cModal.classList.add('is-active');
+});
 
 //mobile
-if(document.getElementById('burger')){
-  const burger = document.getElementById('burger')
-  const menu = document.getElementById('navbar-menu')
+if(document.querySelector('#burger')){
+  const burger = document.querySelector('#burger');
+  const menu = document.querySelector('#navbar-menu');
 
-  burger.addEventListener('click', () => {
-    if (menu.classList.contains('is-active')) {
-      burger.classList.remove('is-active')
-      menu.classList.remove('is-active')
-      return
-    }
-    burger.classList.add('is-active')
-    menu.classList.add('is-active')
-  })
+  burger.addEventListener('click', ({ target }) => {
+    target.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+  });
 }
